a57648b34f852576a4c91125cacf7b8beb12cf1d
Przemek Spychalski <przemek.spychalski@ug.edu.pl>
2018-06-11 13:40:10 +0200

    Add more useful verion of fucntion string_to_alpha_table

--- route_transposition_cipher.py
+++ route_transposition_cipher.py
@@ -5,16 +5,32 @@ DATA_1 = 'This is an example'
 
 DATA_2 = 'why is this professor so boring omg'
 
+DATA_3 = 'For lunch let\'s have peanut-butter and bologna sandwiches'
 
-# TODO write a unite test for this function
-def string_to_alpha_table(text: str, xdim: int, ydim: int):
+
+def string_to_alpha_table1(text: str, xdim: int, ydim: int):
 
     table_to_partition = [e.upper() for e in text if e.isalpha()]
 
     table_to_partition_length = len(table_to_partition)
     dimension = xdim * ydim
 
-    assert table_to_partition_length < dimension
+    if table_to_partition_length < dimension:
+
+        supplementary_table = ['X'] * (dimension - table_to_partition_length)
+
+        table_to_partition.extend(supplementary_table)
+
+    return table_to_partition
+
+
+# TODO write a unite test for this function
+def string_to_alpha_table2(text: str, xdim: int, ydim: int):
+
+    table_to_partition = [e.upper() for e in text if e.isalpha()]
+
+    table_to_partition_length = len(table_to_partition)
+    dimension = xdim * ydim
 
     if table_to_partition_length < dimension:
 
@@ -41,7 +57,11 @@ def encrypt_note(table, algorithm):
 
 
 if __name__ == '__main__':
-    print(string_to_alpha_table(DATA_1, 6, 3))
+    print(string_to_alpha_table2(DATA_1, 6, 3))
 
-    print(string_to_alpha_table(DATA_2, 6, 5))
+    print(string_to_alpha_table2(DATA_2, 6, 5))
+
+    print(string_to_alpha_table2(DATA_3, 4, 12))
+
+    print(string_to_alpha_table1(DATA_3, 4, 12))
 
