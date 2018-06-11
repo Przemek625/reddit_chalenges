56f340710dc4a688647a3abc1a9c669600e37b28
Przemek Spychalski <przemek.spychalski@ug.edu.pl>
2018-06-11 15:37:41 +0200

    Add early version of envrypt algorythm

--- route_transposition_cipher.py
+++ route_transposition_cipher.py
@@ -9,14 +9,12 @@ DATA_3 = 'For lunch let\'s have peanut-butter and bologna sandwiches'
 
 
 def string_to_alpha_table1(text: str, xdim: int, ydim: int):
-
     table_to_partition = [e.upper() for e in text if e.isalpha()]
 
     table_to_partition_length = len(table_to_partition)
     dimension = xdim * ydim
 
     if table_to_partition_length < dimension:
-
         supplementary_table = ['X'] * (dimension - table_to_partition_length)
 
         table_to_partition.extend(supplementary_table)
@@ -26,14 +24,12 @@ def string_to_alpha_table1(text: str, xdim: int, ydim: int):
 
 # TODO write a unite test for this function
 def string_to_alpha_table2(text: str, xdim: int, ydim: int):
-
     table_to_partition = [e.upper() for e in text if e.isalpha()]
 
     table_to_partition_length = len(table_to_partition)
     dimension = xdim * ydim
 
     if table_to_partition_length < dimension:
-
         supplementary_table = ['X'] * (dimension - table_to_partition_length)
 
         table_to_partition.extend(supplementary_table)
@@ -50,18 +46,43 @@ def string_to_alpha_table2(text: str, xdim: int, ydim: int):
     return table
 
 
+def encrypt(data, xdim, ydim):
+
+    encrypted = []
+    X = 0
+    Z = 0
+    c = 1
+    start, end = xdim - 1, xdim * (ydim - X) - 1
+
+    while(True):
+
+        encrypted.extend(data[start: end: 4])
+
+        start, end = end, end - xdim + 1
+        encrypted.extend(data[start: end: -1])
+
+        start, end = end, end - xdim * (ydim - 1 - X)
+        encrypted.extend(data[start: end: -4])
+
+        start, end = end, end + xdim - 1 - 1
+        encrypted.extend(data[start: end: 1])
+        start, end = end, xdim * (ydim - X) - 1
+        print(start, end)
+
+        X += 1
+        c += 1
+        print(encrypted)
+
+        if (c == 3):
+            break
+
+    return encrypted
+
+
 # TODO in the future change the signature of method to take string instead of table.
 def encrypt_note(table, algorithm):
-
     assert algorithm in ['clockwise', 'counterclockwise']
 
 
 if __name__ == '__main__':
-    print(string_to_alpha_table2(DATA_1, 6, 3))
-
-    print(string_to_alpha_table2(DATA_2, 6, 5))
-
-    print(string_to_alpha_table2(DATA_3, 4, 12))
-
-    print(string_to_alpha_table1(DATA_3, 4, 12))
-
+    print(encrypt(string_to_alpha_table1(DATA_3, 4, 12), 4, 12))
