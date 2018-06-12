a259272979126690c079c4a25515d6c908a6f596
Przemek <przemek625@gmail.com>
2018-06-12 23:07:40 +0200

    Add solution

--- i_before_E_except_after_C.py
+++ i_before_E_except_after_C.py
@@ -1,6 +1,19 @@
 # https://www.reddit.com/r/dailyprogrammer/comments/8q96da/20180611_challenge_363_easy_i_before_e_except/
 # Challenge #363 [Easy] I before E except after C
 
+TEST_DATA = [
+    'a',
+    'zombie',
+    'transceiver',
+    'veil',
+    'icier'
+]
+
+
+def follow_spelling_rule(text: str) -> bool:
+    return 'cie' not in text and text.count('ei') == text.count('cei')
+
 
 if __name__ == '__main__':
-    pass
+    for e in TEST_DATA:
+        print(follow_spelling_rule(e))
