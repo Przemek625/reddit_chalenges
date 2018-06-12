36efa75a9cfa0742be84540c46502e417ade7ae0
Przemek Spychalski <przemek.spychalski@ug.edu.pl>
2018-06-12 13:10:27 +0200

    Add project info

--- i_before_E_except_after_C.py
+++ i_before_E_except_after_C.py
@@ -1,2 +1,6 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/8q96da/20180611_challenge_363_easy_i_before_e_except/
+# Challenge #363 [Easy] I before E except after C
+
+
 if __name__ == '__main__':
     pass
