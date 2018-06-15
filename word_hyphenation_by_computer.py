15db64d061669d94a6def0266ed42e705f456677
Przemek Spychalski <przemek.spychalski@ug.edu.pl>
2018-06-15 14:37:28 +0200

    Add task info

---
+++ word_hyphenation_by_computer.py
@@ -0,0 +1,5 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/8qxpqd/20180613_challenge_363_intermediate_word/
+# Challenge #363 [Intermediate] Word Hy-phen-a-tion By Com-put-er
+
+if __name__ == '__main__':
+    pass
