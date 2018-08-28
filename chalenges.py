8381ae5ac85e7e4892a116b0ddb1ead91d711d22
Przemek Spychalski <przemek625@gmail.com>
2018-08-28 08:29:17 +0200

    Add chalenge

--- chalenges.py
+++ chalenges.py
@@ -18,5 +18,25 @@ def generuj_kody(kod1: str, kod2: str):
 
     return zakres_kodow
 
+
+def zwroc_brakujace_elementy(itterable: list, n: int)->list:
+
+    A = set(range(1, n + 1))
+    B = set(itterable)
+
+    return list(A - B)
+
+
+def generuj_liste_liczb_ze_skokiem(start, end, step):
+    return [start + x * step for x in range(0, int((end - start) / step) + 1)]
+
+
 if __name__ == '__main__':
-    generuj_kody('79-900', '80-155')
+    v1 = [2, 3, 7, 4, 9]
+    n1 = 10
+
+    print(zwroc_brakujace_elementy(v1, n1))
+
+    print(generuj_kody('79-900', '80-155'))
+
+    print(generuj_liste_liczb_ze_skokiem(2.0, 5.5, 0.5))
